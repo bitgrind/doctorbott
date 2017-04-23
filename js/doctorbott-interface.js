17b9d94f0bdfd2c54e34fd34685fc4603842544a
Keith Stedman <keithstedman@gmail.com>
2017-04-23 11:10:46 -0700

    adding html to accopany js results

--- js/doctorbott-interface.js
+++ js/doctorbott-interface.js
@@ -1,17 +1,30 @@
-var doctorbott = require('./../js/doctorbott.js').DocBott;
+var DocBott = require('./../js/doctorbott.js').DocBott;
+var apiKeyDoctorBott = require('./../.env').apiKeyDoctorBott;
 
+
+
+var displayIllness = function(response) {
+  var illness = response.data;
+  console.log("display illness");
+  console.log(illness);
+  illness.forEach(function(item){
+    $("#health-output").append();
+    console.log(item);
+  });
+}
 
 $(function() {
   console.log("doc rdy");
-  $("#search").submit(function(){
+  $("#getDoctors").click(function(){
     event.preventDefault();
     console.log("query");
-    var newDocBot = new DocBott();
-    newDocBot.getIllness(input);
+    //var newDocBot = new DocBott();
+    //newDocBot.getIllness();
+
+    $.get('https://api.betterdoctor.com/2016-03-01/doctors?location=37.773,-122.413,10&skip=2&limit=10&user_key=' + apiKeyDoctorBott)
+      .then(function(response) {
+        console.log("response"+response);
+        displayIllness(response);
+    });
   });
-
-
-  var displayIllness = function(response) {
-    console.log(response);
-  }
 });
--- js/doctorbott.js
+++ js/doctorbott.js
@@ -6,19 +6,28 @@ var apiKeyDoctorBott = require('./../.env').apiKeyDoctorBott;
 
 var resource_url = 'https://api.betterdoctor.com/2016-03-01/practices?location=37.773,-122.413,10&skip=2&limit=10&user_key=' + apiKeyDoctorBott;
 
+var resource_url = 'https://api.betterdoctor.com/2016-03-01/doctors?location=37.773,-122.413,10&skip=2&limit=10&user_key=' + apiKeyDoctorBott;
+
 
 //constrcutor
 function DocBott() {
 }
 
 //Methods
-DocBott.prototype.getIllness = function(input) {
-  $.get(resource_url)
+DocBott.prototype.getIllness = function() {
+  $.get('https://api.betterdoctor.com/2016-03-01/practices?location=37.773,-122.413,10&skip=2&limit=10&user_key=' + apiKeyDoctorBott)
     .then(function(response) {
-      console.log(response);
+      console.log("response"+response);
       displayIllness(response);
   });
+}
 
+DocBott.prototype.getDoctors = function() {
+  $.get(resource_url)
+    .then(function(response) {
+      console.log("response"+response);
+      displayDoctors(response);
+  });
 }
 
 //Exports
